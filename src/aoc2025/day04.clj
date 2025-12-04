8cbaf9d38e42929db19119b7a46d6c62a71d9b41
Alexandar Guzsvany <git@guzsvany.de>
2025-12-04 17:21:14 +0100

    Day 4: Printing Department, parts 1 and 2
    Rework of part 1 to use the same data structures as part 2, namely a set for
    the rolls instead of a vec of strings.

--- src/aoc2025/day04.clj
+++ src/aoc2025/day04.clj
@@ -1,56 +1,67 @@
 (ns aoc2025.day04
   (:require
+    [clojure.set :as set]
     [clojure.string :as str]
     [aoc2025.util :as util]))
 
 ;;; day 4: Printing Department
 
 (defn parse-input
-  "Returns input as vec of lines."
+  "Returns input as set of [r c] of paper rolls (i.e. @ chars)."
   []
   (->> (util/get-input *ns*)
        (str/split-lines)
-       vec))
+       (util/parse-area {:ignore-chars #{\.}})
+       keys
+       set))
+
+(defn get-adjacent
+  "Returns rolls adjacent to the given location, as set of [r c] pairs."
+  [rolls [r c]]
+  (->> ; construct locations to check...
+       (for [r' (range (dec r) (+ r 2))
+             c' (range (dec c) (+ c 2))
+             :when (or (not= r r') (not= c c'))]
+         [r' c'])
+       ; ...and check them within `rolls`
+       (filter rolls)))
+
+(defn find-accessable-rolls
+  "Returns all accessable rolls in `rolls`, as a seq of [r c] pairs, or
+  nil if no such rolls exist.
+  A roll is accessable if it has less than 4 adjacent rolls."
+  [rolls]
+  (->> rolls
+       (filter (fn[rc] (-> rolls (get-adjacent rc) count (< 4))))
+       seq))
 
 ;; part 1
 
-(defn roll-at?
-  "Returns if the input at given row and column is a 'roll' (i.e. @ sign)."
-  [input r c]
-  (= \@ (get-in input [r c])))
-
-(defn get-adjacent-rolls
-  "Returns seq of [r c] pairs, the adjacent rolls of the given location."
-  [input r c]
-  (for [r' (range (dec r) (+ r 2))
-        c' (range (dec c) (+ c 2))
-        :when (and
-                (or (not= r r') (not= c c'))
-                (roll-at? input r' c'))]
-    [r' c']))
-
 (defn part-1
   []
-  (let [input (parse-input)
-        height (count input)
-        width (count (first input))
-        rolls (set (for [c (range width)
-                         r (range height)
-                         :when (roll-at? input r c)
-                         :let [ars (get-adjacent-rolls input r c)]
-                         :when (-> ars count (< 4))]
-                     [r c]))]
-    (util/plot-maze [height width] #(if (rolls %) \x (get-in input %))
-                    :out "var/map-5.txt")
-    (count rolls)))
-
+  (-> (parse-input)
+      (find-accessable-rolls)
+      count))
 
 ;; part 2
 
+(defn remove-accessable-rolls
+  "From the given `rolls` set removes all accessable rolls and returns the
+  resulting rolls set as well as the number of removed rolls, as map
+  {:rolls <remaining rolls> :removed <num of removed rolls>}, or nil if
+  there are no accessable rolls left in `rolls` (i.e nothing to remove)."
+  [{:keys [rolls]}]
+  (if-let [acc-rolls (find-accessable-rolls rolls)]
+    {:rolls (->> rolls (remove (set acc-rolls)) set)
+     :removed (count acc-rolls)}))
+
 (defn part-2
   []
-  (->> (parse-input)
-       count))
+  (->> {:rolls (parse-input) :removed 0}
+       (iterate remove-accessable-rolls)
+       (take-while identity)
+       (map :removed)
+       (reduce +)))
 
 (comment
   (set! *warn-on-reflection* true)
