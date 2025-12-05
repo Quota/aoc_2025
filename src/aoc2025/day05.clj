a508dd99213cef0960faeaa3745a9cf1d662b5b3
Alexandar Guzsvany <git@guzsvany.de>
2025-12-05 21:27:39 +0100

    Day 5: Cafeteria, part 2

--- src/aoc2025/day05.clj
+++ src/aoc2025/day05.clj
@@ -3,7 +3,7 @@
     [clojure.string :as str]
     [aoc2025.util :as util]))
 
-;;; day 5:
+;;; day 5: Cafeteria
 
 (defn parse-input
   "Returns input as map {:ranges <seq of [low high]> :numbers <seq...>}."
@@ -19,6 +19,7 @@
 ;; part 1
 
 (defn in-ranges?
+  "Returns if the given number is contained in any of the ranges."
   [ranges n]
   (some (fn[[low high]] (<= low n high)) ranges))
 
@@ -29,15 +30,30 @@
          (filter #(in-ranges? ranges %))
          count)))
 
-
 ;; part 2
+
+(defn add-range
+  "Adds `next-range` to `ranges`, shortening or even omitting it if necessary
+  to avoid overlapping."
+  [ranges [next-low next-high :as next-range]]
+  (let [[_ prev-high] (peek ranges)]
+    (cond
+      ; initial
+      (nil? prev-high) [next-range]
+      ; next range after prev range:
+      (> next-low prev-high) (conj ranges next-range)
+      ; partial overlap:
+      (> next-high prev-high) (conj ranges [(inc prev-high) next-high])
+      ; next range *inside* prev range:
+      :otherwise ranges)))
 
 (defn part-2
   []
   (let [{:keys [ranges]} (parse-input)]
     (->> ranges
-         ; todo: overlapping ranges...
-         (map (fn[[low high]] (inc (- high low))))
+         sort
+         (reduce add-range [])
+         (map (fn[[l h]] (-> h (- l) inc)))
          (reduce +))))
 
 (comment
